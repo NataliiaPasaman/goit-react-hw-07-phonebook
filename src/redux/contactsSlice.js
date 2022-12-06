52351c046a3c49f4d83341cff98f16d6c42f63e2
NataliiaPasaman <natapasaman@gmail.com>
2022-12-06 23:20:50 +0200

    delete saved to LocalStorage

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 export const contactsSlice = createSlice({
     name: 'contacts',
@@ -16,10 +14,5 @@ export const contactsSlice = createSlice({
     },
 });
 
-const persistConfig = {
-    key: 'contacts',
-    storage,
-  }
-export const persistedContactsReducer = persistReducer(persistConfig, contactsSlice.reducer);
-
 export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
