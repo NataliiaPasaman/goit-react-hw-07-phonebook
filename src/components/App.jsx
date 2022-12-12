bb9f119b39eaced3ea24710376e278e7f870dddc
NataliiaPasaman <natapasaman@gmail.com>
2022-12-12 18:45:28 +0200

    render contacts from API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { useDispatch } from "react-redux";
+import { selectIsLoading } from 'redux/selector';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch])
+
+  console.log(selectIsLoading, '______selectIsLoading');
 
   return (
     <div
