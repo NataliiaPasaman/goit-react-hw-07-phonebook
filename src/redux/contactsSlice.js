139a7e56719772893976c5fbe447c73c5a957d71
NataliiaPasaman <natapasaman@gmail.com>
2022-12-12 18:44:57 +0200

    add reducers when go to get contacts from API

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts } from 'redux/operations';
 
 export const contactsSlice = createSlice({
     name: 'contacts',
@@ -7,16 +8,39 @@ export const contactsSlice = createSlice({
           isLoading: false,
           error: null,
       },
-    reducers: {
-        addContact (state, action) {
-                state.items.unshift(action.payload);
-            },
-        deleteContact (state, action) {
-            const index = state.items.findIndex(contact => contact.id === action.payload);
-            state.items.splice(index, 1);
-        }
+    extraReducers: {
+        [fetchContacts.pending] (state) {
+            state.isLoading = true;
+        },
+        [fetchContacts.fulfilled] (state, action) {
+            state.isLoading = false;
+            state.items = action.payload;
+            state.error = null;
+        },
+        [fetchContacts.rejected] (state, action) {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
     },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
+// export const contactsSlice = createSlice({
+//     name: 'contacts',
+//     initialState: {
+//           items: [],
+//           isLoading: false,
+//           error: null,
+//       },
+//     reducers: {
+//         addContact (state, action) {
+//                 state.items.unshift(action.payload);
+//             },
+//         deleteContact (state, action) {
+//             const index = state.items.findIndex(contact => contact.id === action.payload);
+//             state.items.splice(index, 1);
+//         }
+//     },
+// });
+
+// export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
